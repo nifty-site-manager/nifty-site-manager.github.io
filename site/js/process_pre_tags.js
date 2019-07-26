35bc3667f59a3f26e6f31f619ab54365e9908387
n-ham <nicholas.charles.ham@gmail.com>
2019-07-26 12:20:34 +1000

    fixed up site/js/process_pre_tags.js

--- site/js/process_pre_tags.js
+++ site/js/process_pre_tags.js
@@ -2,24 +2,25 @@
 	Has browser ignore leading tabs for <pre> code blocks.
 	modified from: http://stackoverflow.com/a/8505182/4525897
 */
-/*$('pre').html(function() {
-    return this.innerHTML.replace(/&nbsp;&nbsp;&nbsp;&nbsp;/g, '\t');
-});*/
-
 var pre_elements = document.getElementsByTagName('pre');
-
 for (var i = 0; i < pre_elements.length; i++)
 {
 	pre_elements = document.getElementsByTagName('pre');
 	var content = pre_elements[i].innerHTML;
 
-	/*content = content.replace(/    /g, '\t');*/
-
 	var tabs_to_remove = '';
-	while (content.indexOf('\t') == '0')
+	while(content.indexOf('\t') == '0' || content.substring(0, 4) == "    ")
 	{
-		tabs_to_remove += '\t';
-		content = content.substring(1);
+		if(content.indexOf('\t') == '0')
+		{
+			tabs_to_remove += '\t';
+			content = content.substring(1);
+		}
+		else
+		{
+			tabs_to_remove += "    ";
+			content = content.substring(4);
+		}
 	}
 
 	var re = new RegExp('\n' + tabs_to_remove, 'g');
