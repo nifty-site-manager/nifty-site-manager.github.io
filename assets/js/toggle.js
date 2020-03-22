170e2c3cf527f7f820e27335ea85cc35bf53b65d
n-ham <nicholas.charles.ham@gmail.com>
2020-03-23 05:23:55 +1100

    more updates for v2.1

---
+++ assets/js/toggle.js
@@ -0,0 +1,52 @@
+var dark_mode = 0;
+
+function toggle_light_dark() {
+
+  if(document.getElementById("main"))
+    document.getElementById("main").classList.toggle("dark-mode");
+
+  var elements = document.getElementsByClassName("content");
+  if(elements.length)
+    elements[0].classList.toggle("dark-mode");
+
+  elements = document.getElementsByClassName("title");
+  if(elements.length)
+    elements[0].classList.toggle("dark-mode");
+
+  if(document.getElementById("details-title"))
+    document.getElementById("details-title").classList.toggle("dark-mode");
+
+  elements = document.getElementsByTagName("h1");
+  for(var i=0; i<elements.length; i++)
+    elements[i].classList.toggle("dark-mode");
+
+  elements = document.getElementsByTagName("h2");
+  for(var i=0; i<elements.length; i++)
+    elements[i].classList.toggle("dark-mode");
+
+  elements = document.getElementsByTagName("h3");
+  for(var i=0; i<elements.length; i++)
+    elements[i].classList.toggle("dark-mode");
+
+  elements = document.getElementsByTagName("h4");
+  for(var i=0; i<elements.length; i++)
+    elements[i].classList.toggle("dark-mode");
+
+  elements = document.getElementsByTagName("th");
+  for(var i=0; i<elements.length; i++)
+    elements[i].classList.toggle("th-dark");
+
+  elements = document.getElementsByTagName("td");
+  for(var i=0; i<elements.length; i++)
+    elements[i].classList.toggle("td-dark");
+
+  dark_mode = (dark_mode+1)%2;
+  localStorage.setItem('dark-mode', dark_mode);
+}
+
+if(localStorage.getItem('dark-mode') && localStorage.getItem('dark-mode') === "1") 
+{
+  toggle_light_dark();
+  document.getElementById("light_dark_LHS").innerHTML = "dark";
+  document.getElementById("light_dark_RHS").innerHTML = "light";
+}
