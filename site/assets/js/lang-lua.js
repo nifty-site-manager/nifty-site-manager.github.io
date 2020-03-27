2ad5ae26f923120d6ac02bde228b112afe8dc9d9
n-ham <nicholas.charles.ham@gmail.com>
2020-03-27 22:59:43 +1100

    more updates and improvements

---
+++ site/assets/js/lang-lua.js
@@ -0,0 +1,77 @@
+/**
+ * @license
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview
+ * Registers a language handler for Lua.
+ *
+ *
+ * To use, include prettify.js and this file in your HTML page.
+ * Then put your code in an HTML tag like
+ *      <pre class="prettyprint lang-lua">(my Lua code)</pre>
+ *
+ *
+ * I used http://www.lua.org/manual/5.1/manual.html#2.1
+ * Because of the long-bracket concept used in strings and comments, Lua does
+ * not have a regular lexical grammar, but luckily it fits within the space
+ * of irregular grammars supported by javascript regular expressions.
+ *
+ * @author mikesamuel@gmail.com
+ */
+
+PR['registerLangHandler'](
+    PR['createSimpleLexer'](
+        [
+         // Whitespace
+         [PR['PR_PLAIN'],       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
+         // A double or single quoted, possibly multi-line, string.
+         [PR['PR_STRING'],      /^(?:\"(?:[^\"\\]|\\[\s\S])*(?:\"|$)|\'(?:[^\'\\]|\\[\s\S])*(?:\'|$))/, null, '"\'']
+        ],
+        [
+         // /*comments*/
+         [PR['PR_COMMENT'], /^\/\*[\s\S]*?(?:\*\/|$)/, null],
+         // //comments
+         [PR['PR_COMMENT'], /^\/\/[\s\S]*?(?:\n|$)/, null],
+         // #comments
+         [PR['PR_COMMENT'], /^\#[\s\S]*?(?:\n|$)/, null],
+         // --[[comments]]--
+         [PR['PR_COMMENT'], /^--\[\[[\s\S]*?(?:\]\]--|$)/, null],
+         // --comments
+         [PR['PR_COMMENT'], /^--[\s\S]*?(?:\n|$)/, null],
+         // @---comments@---
+         [PR['PR_COMMENT'], /^\@---[\s\S]*?(?:\@---|$)/, null],
+         
+         // A long bracketed block not preceded by -- is a string
+         [PR['PR_STRING'],  /^\[(=*)\[[\s\S]*?(?:\]\1\]|$)/],
+         [PR['PR_KEYWORD'], /^(?:and|break|console|const|do|else|elseif|end|endl|exit|false|for|function|if|in|local|nil|not|option|options|or|param|params|print|private|quit|repeat|return|require|then|true|until|while)\b/, null],
+         [PR['PR_LITERAL'], /^(?:lua|lua_addnsmfns|lua_getglobal|lua_gettop|lua_pop|lua_pushlightuserdata|lua_pushnumber|lua_pushstring|lua_remove|lua_setglobal|lua_tonumber|lua_tostring|nsm_setnumber|nsm_setstring|nsm_tolightuserdata|nsm_tonumber|nsm_tostring|nsm_write|:=)\b/, null],
+         [PR['PR_TYPE'], /^(?:bool|char|double|fstream|ifstream|int|iterable|long long int|number|ofstream|path|source|string|target|var|variable|std::bool|std::char|std::double|std::int|std::long long int|std::string)\b/, null],
+         // operations and order relations
+         [PR['PR_DECLARATION'], /^(?:->|<-|<=|>=)/, null],
+         // := and :
+         [PR['PR_LITERAL'], /^(?::=|:)/, null],
+         // n++ functions
+         [PR['PR_LITERAL'], /^@(?:\[(=*)\[[\s\S]*?(?:\]\1\]|$)|[^ ({[\r\n]*)/],
+         // A number is a hex integer literal, a decimal real literal, or in scientific notation
+         [PR['PR_TYPE'],
+          /^[+-]?(?:0x[\da-f]+|(?:(?:\.\d+|\d+(?:\.\d*)?)(?:e[+\-]?\d+)?))/i],
+         // An identifier
+         [PR['PR_PLAIN'], /^[a-z_]\w*/i],
+         // A run of punctuation
+         [PR['PR_PUNCTUATION'], /^[^\w\t\n\r \xA0][^\w\t\n\r \xA0\"\'\-\+=]*/]
+        ]),
+    ['lua']);
