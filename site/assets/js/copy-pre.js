3881106493d38cf65f642a4f79412b08c541c777
n-ham <nicholas.charles.ham@gmail.com>
2020-03-28 08:37:42 +1100

    finished adding docs for hard-coded functions

--- site/assets/js/copy-pre.js
+++ site/assets/js/copy-pre.js
@@ -1,14 +1,36 @@
 var pre_blocks = document.getElementsByTagName("pre");
 var pre_blocks_txt = [];
 
-for(var i=0; i<pre_blocks.length; i++)
+function load() 
 {
-	/*alert(pre_blocks[i].innerHTML);*/
-	while(pre_blocks[i].innerHTML[pre_blocks[i].innerHTML.length-1] == '\n')
-		pre_blocks[i].innerHTML = pre_blocks[i].innerHTML.substring(0, pre_blocks[i].innerHTML.length-1);
-	pre_blocks_txt.push(pre_blocks[i].innerHTML);
-	pre_blocks[i].innerHTML = pre_blocks[i].innerHTML + "<noselect><cpbutton onclick='copy_pre_block(" + i + ")'><img src='../../images/copy.png' width='25px' style='vertical-align:middle'></cpbutton></noselect>";
+    var img = new Image();
+    var cpsrc = "images/copy.png";
+    var depth = 0;
+
+    img.onload = function()
+    {
+    	for(var i=0; i<pre_blocks.length; i++)
+		{
+			//alert(pre_blocks[i].innerHTML);
+			while(pre_blocks[i].innerHTML[pre_blocks[i].innerHTML.length-1] == '\n')
+				pre_blocks[i].innerHTML = pre_blocks[i].innerHTML.substring(0, pre_blocks[i].innerHTML.length-1);
+			pre_blocks_txt.push(pre_blocks[i].innerHTML);
+
+			pre_blocks[i].innerHTML = pre_blocks[i].innerHTML + "<noselect><cpbutton onclick='copy_pre_block(" + i + ")'><img src='" + img.src + "' width='25px' style='vertical-align:middle'></cpbutton></noselect>";
+		}
+    }; 
+    img.onerror = function()
+    {
+    	if(++depth > 15)
+    		return;
+    	cpsrc = "../" + cpsrc;
+    	img.src = cpsrc;
+    };
+
+    img.src = cpsrc;
 }
+
+load();
 
 function copy_pre_block(i)
 {
