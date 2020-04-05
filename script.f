e21a4cd1cf0a9a1df901dbab0abe5b8b4a18ec64
n-ham <nicholas.charles.ham@gmail.com>
2020-04-06 06:19:15 +1000

    updated website for v2.3.1

--- script.f
+++ script.f
@@ -1,11 +1,20 @@
-nsm cp docs/fns/dep docs/fns/item
-nsm cp docs/fns/dep docs/fns/paginate
-nsm cp docs/fns/dep "docs/fns/paginate\,no_items_per_page"
-touch "content/docs/fns/paginate,no_items_per_page-post-build.f"
-nsm cp docs/fns/dep "docs/fns/paginate\,separator"
-touch "content/docs/fns/paginate,separator-post-build.f"
-nsm cp docs/fns/dep "docs/fns/paginate\,template"
-touch "content/docs/fns/paginate,template-post-build.f"
+nsm mv "docs/fns/#" "docs/fns/HASH"
+nsm mv "docs/fns/#--" "docs/fns/HASH--"
+nsm mv "docs/fns/<#--" "docs/fns/<HASH--"
+nsm mv "docs/fns/%" "docs/fns/MOD"
+nsm mv "docs/fns/%=" "docs/fns/MOD="
+
+
+<#--
+	nsm cp docs/fns/dep docs/fns/item
+	nsm cp docs/fns/dep docs/fns/paginate
+	nsm cp docs/fns/dep "docs/fns/paginate\,no_items_per_page"
+	touch "content/docs/fns/paginate,no_items_per_page-post-build.f"
+	nsm cp docs/fns/dep "docs/fns/paginate\,separator"
+	touch "content/docs/fns/paginate,separator-post-build.f"
+	nsm cp docs/fns/dep "docs/fns/paginate\,template"
+	touch "content/docs/fns/paginate,template-post-build.f"
+--#>
 
 <#--
 	nsm mv "docs/fns/\@---" docs/fns/\#--
